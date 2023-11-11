33d98a2366fb0a53ad3de69603fdfd1f80a74f39
Yan Lytovka <retraut@gmail.com>
2023-11-11 19:27:43 +0100

    update with fonts and page number

--- app.py
+++ app.py
@@ -1,59 +1,53 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import os
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 # Port
 port = os.getenv('PORT')
 log_file = os.getenv('LOG_FILE')
 
-# Initialize the current line number as a global variable
-current_line_number = 0
+# Initialize the current page number and lines per page as global variables
+current_page_number = 1
+lines_per_page = 100
 
 @app.route('/')
-def display_code():
-    global current_line_number
+@app.route('/page/<int:page_number>')
+def display_code(page_number=1):
+    global current_page_number
+    current_page_number = page_number
+
     # Read the code file (change the path to your file)
     with open(log_file, 'r') as file:
         code_lines = file.readlines()
 
-    # Determine the range of lines to display (100 lines at a time)
-    if current_line_number > 0:
-        start_line = current_line_number
-        end_line = current_line_number + 100
-        current_code = '\n'.join(code_lines[start_line:end_line])
-    else:
-        current_code = '\n'.join(code_lines[-100:])
-    return render_template('code.html', code=current_code, current_line_number=current_line_number)
+    # Determine the range of lines to display based on the current page
+    start_line = (current_page_number - 1) * lines_per_page
+    end_line = start_line + lines_per_page
+    current_code = '\n'.join(code_lines[start_line:end_line])
+
+    return render_template('code.html', code=current_code, current_page_number=current_page_number)
 
 @app.route('/next')
 def next_lines():
-    global current_line_number
-    with open(log_file, 'r') as file:
-        code_lines = file.readlines()
-        max_code_lines = len(code_lines)
-    current_line_number += 100
-    if current_line_number >= max_code_lines:
-       message = "the end of file, see the last 100 strings"
-       current_line_number = max_code_lines - 100
-    return display_code()
+    global current_page_number
+    current_page_number += 1
+    return redirect(url_for('display_code', page_number=current_page_number))
 
 @app.route('/previous')
 def previous_lines():
-    global current_line_number
-    current_line_number -= 100
-    if current_line_number < 0:
-        current_line_number = 0
-    return display_code()
+    global current_page_number
+    current_page_number = max(1, current_page_number - 1)
+    return redirect(url_for('display_code', page_number=current_page_number))
 
 @app.route('/end')
 def to_the_end():
-    global current_line_number
+    global current_page_number
     with open(log_file, 'r') as file:
         code_lines = file.readlines()
-        max_code_lines = len(code_lines)
-    current_line_number = max_code_lines - 100
-    return display_code()
+        max_pages = (len(code_lines) - 1) // lines_per_page + 1
+    current_page_number = max_pages
+    return redirect(url_for('display_code', page_number=current_page_number))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=port)
+    app.run(host="0.0.0.0", port=port)
--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+cat ~/.github_token | docker login ghcr.io -u retraut --password-stdin
 docker build . -t ghcr.io/retraut/python-log-reader:latest
 docker push ghcr.io/retraut/python-log-reader:latest
